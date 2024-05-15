bd1efd763004f253100adad89c596176d755a802
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-05-15 16:42:57 -0600

    Final commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# app.py
+
+from flask import Flask, render_template
+import config
+from models import Person
+# print("Database Object: ",config.db)
+
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
+
+@app.route("/")
+def home():
+    people = Person.query.all()
+    return render_template("home.html", people=people)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+    
